43de78ba5423420e0dfb68341b059edc448294ba
erenymo <erenbalta342115@gmail.com>
2023-10-12 20:44:42 +0300

    latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
     extend: {
       height: {
@@ -11,6 +12,8 @@ module.exports = {
         text: "hsl(200, 15%, 8%)",
         background: "hsl(0, 0%, 98%)",
         elements: "hsl(0, 0%, 100%)",
+        darkModeElements: "hsl(209, 23%, 22%)",
+        darkModeBackground: "hsl(207, 26%, 17%)",
       },
       fontFamily: {
         primary: ["Nunito Sans", "sans-serif"],
